113f5246b699ae39fea985e6bc17b777497e8b77
Mustafa Akın <mustafa91@gmail.com>
2020-04-15 18:53:50 +0300

    Yeni özellik eklendi

--- 01-imaj/server.go
+++ 01-imaj/server.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Merhaba dünya")
+		fmt.Fprintf(w, "Merhaba Mobilhanem")
 	})
 	http.ListenAndServe(":8080", nil)
 }
